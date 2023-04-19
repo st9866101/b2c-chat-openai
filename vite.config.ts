c77ad07c44afe91f009907711a025a1c0e0cc7b5
st9866101 <st9866101@gmail.com>
2023-04-19 18:21:26 +0800

    1.更改路徑

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/b2c-chat-openai/',
   plugins: [react()],
 })
 // /b2c-chat-openai/
